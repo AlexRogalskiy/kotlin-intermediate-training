bd2a61462d34485f1916f5b038e914afab285449
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2023-06-27 11:50:44 +0300

    jpanel

--- src/main/kotlin/com/rockthejvm/practice/App.kt
+++ src/main/kotlin/com/rockthejvm/practice/App.kt
@@ -1,15 +1,46 @@
 package com.rockthejvm.practice
 
+import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.WindowConstants
+import java.awt.Dimension
+import java.awt.Graphics
+import java.awt.image.BufferedImage
+
 object App {
+    private lateinit var imagePanel: ImagePanel
+
+    class ImagePanel(private var image: Image) : JPanel() {
+        override fun paintComponent(g: Graphics) {
+            super.paintComponent(g)
+            g.drawImage(image.bfImage, 0, 0, null)
+        }
+
+        override fun getPreferredSize(): Dimension {
+            return Dimension(image.width, image.height)
+        }
+
+        fun updateImage(newImage: Image) {
+            image = newImage
+            preferredSize = Dimension(newImage.width, newImage.height)
+            revalidate()
+            repaint()
+        }
+    }
+
+    fun load(path: String) {
+        val image = Image(path)
+        val frame = JFrame("Image Display")
+        imagePanel = ImagePanel(image)
+
+        frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+        frame.contentPane.add(imagePanel)
+        frame.pack()
+        frame.isVisible = true
+    }
+
     @JvmStatic
     fun main(args: Array<String>) {
-        val paris = Image("src/main/resources/pictures/paris.jpeg")
-        val gradient = Image("src/main/resources/pictures/redblue.jpg")
-
-        val combined = Blend(gradient, Multiply()).process(paris)
-        combined.write("src/main/resources/results/paris-red.jpg")
-
-        val cropped = Crop(400, 400, 500, 500).process(paris)
-        cropped.write("src/main/resources/results/paris-cropped.jpg")
+        load("src/main/resources/pictures/paris.jpeg")
     }
 }
