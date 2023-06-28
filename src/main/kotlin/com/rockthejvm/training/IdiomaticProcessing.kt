b5eb86d555ac24ad7b89c9a4aa34fc7c175ea0ec
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2023-06-28 11:52:22 +0300

    arrow

---
+++ src/main/kotlin/com/rockthejvm/training/IdiomaticProcessing.kt
@@ -0,0 +1,14 @@
+package com.rockthejvm.training
+
+import arrow.core.*
+
+object IdiomaticProcessing {
+
+    // nullables & Option
+    // Arrow
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+    }
+}
